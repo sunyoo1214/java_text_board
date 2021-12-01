77faf6781b3fad58990d36b6234a7414e199c3b6
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:41:20 +0900

    새 게시물 입력받기 기능 구현 시작

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -15,9 +15,17 @@ public class Main {
 
             if (cmd.equals("exit")) {
                 break;
+            } else if (cmd.equals("/usr/article/write")) {
+                System.out.println("- 게시물 등록 -");
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+                int id = 1;
+                System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
+            } else {
+                System.out.printf("입력된 명령어 : %s\n", cmd);
             }
-
-            System.out.printf("입력된 명령어 : %s\n", cmd);
         }
 
         System.out.println("== 프로그램 종료 ==");
