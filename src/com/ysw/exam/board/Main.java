69edcc9c4ead3b052fc4f97380c01e0e1afe3bd8
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:55:47 +0900

    게시물 리스트에서 출력을 최신순으로

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -12,8 +12,8 @@ import java.util.Scanner;
 public class Main {
     static void makeTestData(ArrayList<Article> articles) {
         articles.add(new Article(1, "제목1", "내용1"));
-        articles.add(new Article(1, "제목2", "내용2"));
-        articles.add(new Article(1, "제목3", "내용3"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
     }
 
     public static void main(String[] args) {
@@ -40,7 +40,8 @@ public class Main {
                 System.out.println("번호 / 제목");
                 System.out.println("--------------------");
 
-                for (Article article : articles) {
+                for (int i = articles.size() - 1; i >= 0; i--) {
+                    Article article = articles.get(i);
                     System.out.printf("%d / %s\n", article.id, article.title);
                 }
 
