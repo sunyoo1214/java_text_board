f89818af639b20ecc106a3b8c03a35a3991ee45a
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:39:06 +0900

    exit가 입력 될 때까지 명령어 계속 입력받기

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -8,9 +8,18 @@ public class Main {
 
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
-        System.out.printf("명령) ");
-        String cmd = sc.nextLine();
-        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+        while (true) {
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("exit")) {
+                break;
+            }
+
+            System.out.printf("입력된 명령어 : %s\n", cmd);
+        }
+
         System.out.println("== 프로그램 종료 ==");
 
         sc.close();
