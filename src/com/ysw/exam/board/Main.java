a97834d37a30a74b2c1324baf02abf882fd0977d
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:35:46 +0900

    시작 알림 메세지 출력

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -1,8 +1,10 @@
 package com.ysw.exam.board;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("하이");
+        System.out.println("== 게시판 v 0.1 ==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.println("명령)");
+        System.out.println("== 프로그램 종료 ==");
     }
 }
