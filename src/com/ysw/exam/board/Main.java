88bd5d1ce0fd5517000e4e82132ac09144938f05
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:50:19 +0900

    /usr/article/detail 명령으로 최근 게시물 보기

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -2,6 +2,12 @@ package com.ysw.exam.board;
 
 import java.util.Scanner;
 
+/*
+### TODO 예시
+- [.] /usr/article/detail 입력처리
+- [ ] /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+*/
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,6 +16,7 @@ public class Main {
         System.out.println("== 프로그램 시작 ==");
 
         int articlesLastId = 0;
+        Article lastArticle = null;
 
         while (true) {
             System.out.printf("명령) ");
@@ -17,6 +24,19 @@ public class Main {
 
             if (cmd.equals("exit")) {
                 break;
+            } else if (cmd.equals("/usr/article/detail")) {
+
+                if (lastArticle == null) {
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
+                Article article = lastArticle;
+
+                System.out.println("- 게시물 상세내용 -");
+                System.out.printf("번호 : %d\n", article.id);
+                System.out.printf("제목 : %s\n", article.title);
+                System.out.printf("내용 : %s\n", article.body);
             } else if (cmd.equals("/usr/article/write")) {
                 System.out.println("- 게시물 등록 -");
                 System.out.printf("제목 : ");
@@ -27,6 +47,7 @@ public class Main {
                 articlesLastId = id;
 
                 Article article = new Article(id, title, body);
+                lastArticle = article;
                 System.out.println("생성된 게시물 객체 : " + article);
 
                 System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
