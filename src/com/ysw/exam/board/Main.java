2319b4ef3dc12782fdbed2a65b014c47f11d45b6
sunyoo1214 <sunyoo1214@gmail.com>
2021-12-01 21:54:18 +0900

    테스트 게시물 생성 함수 적용

--- src/com/ysw/exam/board/Main.java
+++ src/com/ysw/exam/board/Main.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
 */
 
 public class Main {
+    static void makeTestData(ArrayList<Article> articles) {
+        articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(1, "제목2", "내용2"));
+        articles.add(new Article(1, "제목3", "내용3"));
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -20,11 +26,7 @@ public class Main {
         Article lastArticle = null;
         ArrayList<Article> articles = new ArrayList<Article>();
 
-        // 테스트 데이터 3개 등록, 시작
-        articles.add(new Article(1, "제목1", "내용1"));
-        articles.add(new Article(2, "제목2", "내용2"));
-        articles.add(new Article(3, "제목3", "내용3"));
-        // 테스트 데이터 3개 등록, 끝
+        makeTestData(articles);
 
         while (true) {
             System.out.printf("명령) ");
